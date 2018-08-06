c0ed13694a97c5ead98796e58c1aaf945feff380
caohades <cao869637507@qq.com>
2018-08-06 10:37:45 +0800

    微信调试

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cao
+ * Date: 6/8/2018
+ * Time: 10:31 AM
+ */
+
+namespace App\Http\Controllers;
+
+use Log;
+
+class WeChatController extends Controller
+{
+
+    /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $app = app('wechat.official_account');
+        $app->server->push(function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+        return $app->server->serve();
+    }
+}

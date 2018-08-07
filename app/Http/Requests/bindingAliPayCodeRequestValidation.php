be1d3f8f43f27eff5ae58fa902683ab3ae411b7d
caohades <cao869637507@qq.com>
2018-08-07 15:41:22 +0800

    bug

--- app/Http/Requests/bindingAliPayCodeRequestValidation.php
+++ app/Http/Requests/bindingAliPayCodeRequestValidation.php
@@ -4,18 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class bindingAliPayCodeRequestValidation extends FormRequest
+class bindingAliPayCodeRequestValidation extends BaseRequestValidator
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *

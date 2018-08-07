df4c47dd0dc9afa1896d726ba050a7450baaff21
caohades <cao869637507@qq.com>
2018-08-07 15:29:54 +0800

    bug

---
+++ app/Exceptions/RequestValidatorException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pro4
+ * Date: 2017/10/27
+ * Time: 14:21
+ */
+
+namespace App\Exceptions;
+
+
+class RequestValidatorException extends \Exception
+{
+
+}

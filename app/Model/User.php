c850bee811a472ef4fc2da60fcea1b6bba50534f
caohades <cao869637507@qq.com>
2018-08-08 11:46:45 +0800

    fix_bug

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'user';
+    public  $timestamps = false;
     protected $fillable = array('openid', 'head_img','nickname','money','alipay','alipay_name','cumulative_amount','mobile','type');
 }

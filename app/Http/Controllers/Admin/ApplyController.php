d6ab93fc84c55db6792468916a2cd7ae0c3479fd
Seovy <33571736+Seovy@users.noreply.github.com>
2018-08-11 17:33:49 +0800

    /

--- app/Http/Controllers/Admin/ApplyController.php
+++ app/Http/Controllers/Admin/ApplyController.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Input;
  */
 class ApplyController extends Controller
 {
+    protected $exts = ['jpg','jpeg','png','gif'];
+    /**
+     * 应用列表
+     * @return string
+     */
     public function index(){
         $type = Input::get('type', 0);
         $name = Input::get('name', '');
@@ -33,5 +38,80 @@ class ApplyController extends Controller
         }
         $list = $model->paginate(20)->toArray();
         return Response::Success($list);
+    }
+
+    /**
+     * 添加应用
+     */
+    public function add(){
+        $name = Input::get('name', '');
+        $logo = Input::file('logo');
+        $type = Input::get('type', '');
+        $money = Input::get('money', 0);
+        $num = Input::get('num', 0);
+        $rank = Input::get('rank', 0);
+        $note = Input::get('note', '');
+        $pack_name = Input::get('pack_name', '');
+        if (empty($name)){
+            return Response::Error('应用名不能为空');
+        }
+        if (empty($logo)){
+            return Response::Error('应用logo不能为空');
+        }
+        if (empty($num)){
+            return Response::Error('应用分数不能为空');
+        }
+        try{
+            $logo = $this->uploadImg($logo);
+            if (!$logo){
+                return Response::Error('上传logo失败');
+            }
+            $model = new Apply();
+            $data = [
+                'name' => $name,
+                'rank' => $rank,
+                'note' => $note,
+                'money' => $money,
+                'pack_name' => $pack_name,
+                'logo' => $logo,
+                'type' => $type,
+                'status' => 1,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ];
+            $res = $model->insert($data);
+            if ($res){
+                return Response::Success('添加成功');
+            }else{
+                return Response::Error('保存失败');
+            }
+        }catch (\Exception $exception){
+            return response(Response::Error('新增失败'));
+        }
+    }
+
+    /**
+     * 上传图片
+     * @param $img
+     * @return bool|string
+     */
+    protected function uploadImg($img)
+    {
+
+        if(!($img -> isValid())){
+            return false;
+        }
+        $filedir = "/logoImg/"; //定义图片上传路径
+        $imagesName = $img->getClientOriginalName(); //获取上传图片的文件名
+        $extension = $img->getClientOriginalExtension(); //获取上传图片的后缀名
+        $fileName = md5(uniqid($imagesName));
+        $fileName = $fileName . '.' . $extension;//生成新的的文件名
+        $bool = \Storage::disk('logo')->put($fileName, file_get_contents($img->getRealPath()));//
+        $url = $_SERVER['SERVER_NAME'] . $filedir . $fileName;//返回文件路径存贮在数据库
+        if (!$bool) {
+            return false;
+        } else {
+            return $url;
+        }
     }
 }
---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2018/8/11
+ * Time: 13:32
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+/**
+ * 用户控制器
+ * Class UserController
+ * @package App\Http\Controllers\Admin
+ */
+class UserController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}

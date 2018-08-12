eac5927c44610160d83f409f948ef0885629268c
Seovy <33571736+Seovy@users.noreply.github.com>
2018-08-12 10:40:45 +0800

    提现处理

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Controller;
 use App\Libray\Response;
 use App\Model\Income;
 use App\Model\User;
+use App\Model\Withdraw;
 use Illuminate\Support\Facades\Input;
 
 /**
@@ -39,7 +40,18 @@ class UserController extends Controller
         }
         $list = $model->paginate(20)->toArray();
         $try_num = (new Income())->select('user_id',\DB::raw('count(app_id) as try_num'))->groupby('user_id')->get();
-        $list['try_num'] = $try_num;
+        if ($try_num){
+            $try_num = array_column($try_num, 'user_id');
+        }
+        if ($list['data']){
+            foreach ($list['data'] as &$item){
+                if (isset($try_num[$item['user_id']])){
+                    $item['try_num'] = $try_num[$item['user_id']]['try_num'];
+                }else{
+                    $item['try_num'] = 0;
+                }
+            }
+        }
         $android_total = (new User())->where('type', '=', 0)->count('user_id'); //安卓用户总数
         $ios_total = (new User())->where('type', '=', 1)->count(['user_id']); //苹果用户总数
         $list['total_count'] = $android_total + $ios_total; //用户总数
@@ -47,5 +59,35 @@ class UserController extends Controller
         $list['ios_count'] = $ios_total ;
         return Response::Success($list);
 
+    }
+
+    /**
+     * 提现处理列表
+     */
+    public function withdrawList(){
+        $status = Input::get('status', 1); //1为待提现 2为提现成功
+        $nickname = Input::get('nickname', ''); //用户昵称
+        $mobile = Input::get('mobile', ''); //用户手机号码
+        $alipay = Input::get('alipay', ''); //提现账号
+        $where = [];
+        $where[] = ['withdraw.ststus', '=', $status];
+        if ($nickname){
+            $where[] = ['user.nickname', 'like', '%'.$nickname.'%'];
+        }
+        if ($mobile){
+            $where[] = ['user.mobile', 'like', '%'.$mobile.'%'];
+        }
+        if ($alipay){
+            $where[] = ['user.alipay', 'like', '%'.$alipay.'%'];
+        }
+        $model = new Withdraw();
+        $list = $model->getList($where);
+        //待提现总金额
+        $withdraw_count = $model->where('status', '=', $status)->sum('money');
+        //待提现处理总数
+        $people_count = $model->where('status', '=', $status)->count();
+        $list['withdraw_count'] = $withdraw_count;
+        $list['people_count'] = $people_count;
+        return Response::Success($list);
     }
 }

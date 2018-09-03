4ff1e795e78a2ff9b9a5c347675132a609c550d1
HeRedBo <hhbjkd@163.com>
2018-09-03 16:10:50 +0800

    aa

--- app/Libray/Message/CCPRestSmsSDK.php
+++ app/Libray/Message/CCPRestSmsSDK.php
@@ -14,7 +14,7 @@ class CCPRestSmsSDK {
 	private $SoftVersion = '2013-12-26';
 	private $Batch;  //时间戳
 	private $BodyType = "xml";//包体格式，可填值：json 、xml
-	private $enabeLog = true; //日志开关。可填值：true、
+	private $enabeLog = false; //日志开关。可填值：true、
 	private $Filename="./log.txt"; //日志文件
 	private $Handle; 
 	function __construct($ServerIP,$ServerPort,$SoftVersion)	

69ee5ebfce1fbd65ba3c4aacb03a8d77485ecc02
unknown <454103433@qq.com>
2018-09-05 15:58:20 +0800

    123

--- public/h5/applicationDetail/js/smdfqr.js
+++ public/h5/applicationDetail/js/smdfqr.js
@@ -22,6 +22,15 @@
             page:'',//保存分页数据
         },
         methods:{
+            autoLogin: function(opt, cb, cberr) {
+                var that = this;
+                var url = '/auto/login';
+                that.$httpGet(url, opt)
+                    .then(cb, cberr);
+            },
+            autoLoginHandler: function (data) {
+
+            },
             getAppDetail: function(opt, cb, cberr) {
                 var that = this;
                 var url = '/app/list';
@@ -69,7 +78,6 @@
                     that.$myalert(data.msg)
                 }
             },
-
             backLayer: function () {
                 var that = this;
                 console.log(3)
@@ -182,6 +190,79 @@
                 that.loading = true;
                 that.receiveReward(postData,that.receiveRewardHandler)
             },
+            SHA1:function(sIn) {
+
+// SHA1
+                function add(x, y) {
+                    return((x & 0x7FFFFFFF) + (y & 0x7FFFFFFF)) ^ (x & 0x80000000) ^ (y & 0x80000000);
+                }
+
+                function SHA1hex(num) {
+                    var sHEXChars = "0123456789abcdef";
+                    var str = "";
+                    for(var j = 7; j >= 0; j--)
+                        str += sHEXChars.charAt((num >> (j * 4)) & 0x0F);
+                    return str;
+                }
+
+                function AlignSHA1(sIn) {
+                    var nblk = ((sIn.length + 8) >> 6) + 1,
+                        blks = new Array(nblk * 16);
+                    for(var i = 0; i < nblk * 16; i++) blks[i] = 0;
+                    for(i = 0; i < sIn.length; i++)
+                        blks[i >> 2] |= sIn.charCodeAt(i) << (24 - (i & 3) * 8);
+                    blks[i >> 2] |= 0x80 << (24 - (i & 3) * 8);
+                    blks[nblk * 16 - 1] = sIn.length * 8;
+                    return blks;
+                }
+
+                function rol(num, cnt) {
+                    return(num << cnt) | (num >>> (32 - cnt));
+                }
+
+                function ft(t, b, c, d) {
+                    if(t < 20) return(b & c) | ((~b) & d);
+                    if(t < 40) return b ^ c ^ d;
+                    if(t < 60) return(b & c) | (b & d) | (c & d);
+                    return b ^ c ^ d;
+                }
+
+                function kt(t) {
+                    return(t < 20) ? 1518500249 : (t < 40) ? 1859775393 :
+                        (t < 60) ? -1894007588 : -899497514;
+                }
+               var x = AlignSHA1(sIn);
+               var w = new Array(80);
+               var a = 1732584193;
+               var b = -271733879;
+               var c = -1732584194;
+               var d = 271733878;
+               var e = -1009589776;
+               for(var i = 0; i < x.length; i += 16) {
+                   var olda = a;
+                   var oldb = b;
+                   var oldc = c;
+                   var oldd = d;
+                   var olde = e;
+                   for(var j = 0; j < 80; j++) {
+                       if(j < 16) w[j] = x[i + j];
+                       else w[j] = rol(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);
+                       t = add(add(rol(a, 5), ft(j, b, c, d)), add(add(e, w[j]), kt(j)));
+                       e = d;
+                       d = c;
+                       c = rol(b, 30);
+                       b = a;
+                       a = t;
+                   }
+                   a = add(a, olda);
+                   b = add(b, oldb);
+                   c = add(c, oldc);
+                   d = add(d, oldd);
+                   e = add(e, olde);
+               }
+               SHA1Value = SHA1hex(a) + SHA1hex(b) + SHA1hex(c) + SHA1hex(d) + SHA1hex(e);
+               return SHA1Value.toUpperCase();
+            },
         },
         mounted: function() {
             var that = this;
@@ -189,7 +270,18 @@
             that.urlParams = that.$tools.getUrlParams();
             that.ajaxParams.status = that.urlParams.status;
             that.ajaxParams.appId = that.urlParams.id;
-            console.log(that.urlParams)
+            var token = that.urlParams.token;
+            var userId = that.urlParams.userId;
+            var timestamp=new Date().getTime();
+            //自动登录
+            var autoLoginAjaxparams = {
+                token:token,
+                userId:userId,
+                signature:that.SHA1(timestamp+token+userId).toLowerCase(),
+                timestamp:timestamp,
+            }
+            that.autoLogin(autoLoginAjaxparams,that.autoLoginHandler)
+
             //判断时Android还时iOS
             var u = navigator.userAgent;
             that.isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
